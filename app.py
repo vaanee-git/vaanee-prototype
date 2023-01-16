328896656a1421f5198865db50f339b6bba17131
sritej chowdam <sritej@vaanee.io>
2023-01-16 14:50:08 +0530

    Azure STT API

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask import send_file
 from pymongo import MongoClient
-import azure.cognitiveservices.speech as speechsdk
+from datetime import datetime
 import requests
 import time
 
@@ -20,52 +20,24 @@ turn_audio = {"1": "./audio/Greet_default.wav",
               "11": "./audio/Goodbye_default.wav"}
 
 
-@app.route('/bot_response', methods=['GET'])
-def get_bot_response():
-    args = request.args
-    turn = args.get("turn")
-    if int(turn) > 0:
-        if turn in turn_audio.keys():
-            path_to_file = turn_audio[turn]
-            return send_file(
-                path_to_file,
-                mimetype="audio/wav",
-                as_attachment=True)
-    return {"Audio": "NOT FOUND"}
+def get_current_date_time():
+    now = datetime.now()
+
+    # Format : dd/mm/YY H:M:S
+    return now.strftime("%d/%m/%Y %H:%M:%S")
 
 
-def save_to_db(turn, response):
+def save_to_db(call_dict):
     URI = "mongodb+srv://vaanee-sritej:ffng9vl1yE7fGjwl@cluster0.ensbbnz.mongodb.net/?retryWrites=true&w=majority"
     client = MongoClient(URI)
     db = client["prototype"]
     col = db["user_response"]
 
-    mydict = {"turn": int(turn), "response": response}
-    x = col.insert_one(mydict)
+    x = col.insert_one(call_dict)
 
     if x.inserted_id:
         return True
     return False
-
-
-def recognize_from_wav(file_path):
-    speech_config = speechsdk.SpeechConfig(subscription='159f7108b2a74f279eecb004944afd95', region='centralindia')
-    speech_config.speech_recognition_language = "en-IN"
-    audio_config = speechsdk.audio.AudioConfig(filename=file_path)
-    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
-    speech_recognition_result = speech_recognizer.recognize_once_async().get()
-
-    if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
-        return speech_recognition_result.text
-    elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
-        print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
-    elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = speech_recognition_result.cancellation_details
-        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-        if cancellation_details.reason == speechsdk.CancellationReason.Error:
-            print("Error details: {}".format(cancellation_details.error_details))
-            print("Did you set the speech resource key and region values?")
-    return None
 
 
 def recognize_from_api(data):
@@ -77,18 +49,97 @@ def recognize_from_api(data):
     return res.json()["DisplayText"]
 
 
-@app.route('/user_response', methods=['POST'])
-def save_user_response():
-    if request.method == 'POST':
-        turn = request.args.get("turn")
-        audio = request.files['music_file']
-        text = recognize_from_api(audio)
+active_phone_numbers = {}
 
-        # Save to DB
-        start = time.time()
-        # save_to_db(turn=turn, response=text)
-        end = time.time()
-        print(end-start)
+
+@app.route('/register_call_initiation', methods=['POST'])
+def register_call_initiation():
+    if "caller_number" in request.json:
+        caller_number = str(request.json["caller_number"])
+        if len(caller_number) == 10:
+            if caller_number in active_phone_numbers.keys():
+                return {"number_saved": True,
+                        "caller_number": "ALREADY SAVED AS ACTIVE"}
+            else:
+                active_phone_numbers[caller_number] = {"call_start_time": get_current_date_time()}
+                active_phone_numbers[caller_number]["user_responses"] = {}
+            return {"number_saved": True}
+        else:
+            return {"number_saved": False,
+                    "caller_number": "INVALID"}
+    return {"number_saved": False,
+            "caller_number": "NOT_FOUND"}
+
+
+@app.route('/get_active_caller_numbers', methods=['GET'])
+def get_active_caller_numbers():
+    return {"active_caller_numbers": list(active_phone_numbers.keys())}
+
+
+@app.route('/get_bot_response', methods=['GET'])
+def get_bot_response():
+    args = request.args
+    caller_number = args.get("caller_number")
+    if caller_number in active_phone_numbers.keys():
+        turn = args.get("turn")
+        if int(turn) > 0:
+            if turn in turn_audio.keys():
+                path_to_file = turn_audio[turn]
+                return send_file(
+                    path_to_file,
+                    mimetype="audio/wav",
+                    as_attachment=True)
+            else:
+                return {"Audio": "NOT FOUND"}
+        else:
+            return {"invalid_turn_number": True}
+    else:
+        return {"caller_number": "INACTIVE"}
+
+
+@app.route('/send_user_response', methods=['POST'])
+def capture_user_response():
+    if request.method == 'POST':
+        args = request.args
+        turn = args.get("turn")
+        caller_number = str(args.get("caller_number"))
+
+        # get audio and its transcription
+        audio = request.files['user-audio']
+        transcription = recognize_from_api(audio)
+
+        # Save to Memory
+        if caller_number:
+            if caller_number in active_phone_numbers.keys():
+                active_phone_numbers[caller_number]["user_responses"][turn] = transcription
+
         return {"turn": turn,
-                "response": text,
+                "response": transcription,
                 }
+
+
+@app.route('/get_user_responses', methods=['GET'])
+def get_user_responses():
+    args = request.args
+    caller_number = str(args.get("caller_number"))
+    if len(caller_number) == 10:
+        return active_phone_numbers[caller_number]["user_responses"]
+
+
+@app.route('/register_call_ended', methods=['POST'])
+def register_call_ended():
+        audio = request.files['call-recording']
+        args = request.args
+        caller_number = str(args.get("caller_number"))
+        if caller_number and caller_number in active_phone_numbers.keys():
+            call_details = active_phone_numbers[caller_number]
+            call_dict = {"caller_number": caller_number,
+                         "call_start_time": call_details["call_start_time"],
+                         "call_end_time": get_current_date_time(),
+                         "user_responses": call_details["user_responses"]
+                         }
+            save_to_db(call_dict)
+            del active_phone_numbers[caller_number]
+            return {"call_details_saved": True}
+        else:
+            return {"call_not_registered": True}
